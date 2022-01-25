6698b5b6e3e435019ebd3a208423bf9c05a47f89
abdullahi ahmed <aahmed_02@hotmail.com>
2022-01-25 01:07:28 +0000

    fixing char counter after a tweet is made

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,104 +1,96 @@
-$(document).ready(function() {
 
-const fetchTweets= ()=> { 
-    $.ajax({
-      url: "/tweets",
-      method: "GET",
-      success: (data) => {
-        console.log(data)
-        renderTweets(data)
-      },
-  
-      error: (err) => {
-        console.log(err)
-      },
-  
-    });
-  }   
+const fetchTweets = () => {
+  $.ajax({
+    url: "/tweets",
+    method: "GET",
+    dataType: "json",
+    success: function (data) {
+      console.log(data)
+      document.getElementById("error-message").style.display = "none"
+      renderTweets(data)
+    },
+
+    error: (err) => {
+      console.log(err)
+    },
+
+  });
+}
+
+const renderTweets = function (tweets) {
+  console.log(tweets)
+  $("#tweets-container").empty();
+  tweets.forEach(tweet => {
+    const $newTweet = createTweetElement(tweet)
+    $("#tweets-container").prepend($newTweet)
+  });
+
+}
+
+const createTweetElement = function (tweetData) {
+
+  let $tweet = $(`
+      <article class="tweet">
+          <header class="tweet">
+            <div class="tweet">
+              <img height="50px" width="50px" class="avatars" src="${tweetData.user.avatars}">
+              <p>${tweetData.user.name}</p>
+            </div>
+            <p class="handle">${tweetData.user.handle}</p>
+          </header>
+      
+          <main class="content">
+            ${tweetData.content.text}
+          </main>
+      
+          <footer class="tweet">
+            <p class="created_at">${timeago.format(tweetData.created_at)}</p>
+            <div class="icons">
+              <div class="flag"><i class="fas fa-flag"></i></div>
+              <div class="retweet"><i class="fas fa-retweet"></i></div>
+              <div class="like"><i class="fas fa-heart"></i></div>  
+            </div>
+          </footer>
+      
+          </article>
+      `)
+  return $tweet;
+}
+
+// renderTweets(data);
+
+$(document).ready(function () {
   fetchTweets()
   $('.error').hide()
-  
-  $('#form-new-tweet').on('submit', function(event) {
-
+  $('form').on('submit', function (event) {
     event.preventDefault();
     const serializedData = $(this).serialize();
     const tweetLength = $("#tweet-text").val().length
     if (tweetLength > 140) {
       document.querySelector('.error').innerHTML = 'text too long. must be less than 140'
-      $('.error').show() 
+      $('.error').show()
       console.log(" must be less than 140 characters")
-      // alert("ERROR: tweet must be less than 140 characters")
       return false
-    
+
     }
 
     else if ($("#tweet-text").val() === '') {
-      document.querySelector('.error').innerHTML = 'Sorry, Tweet cannot be empty'
-      $('.error').show() 
-      console.log("Sorry, tweet cannot be empty tweet")
-      // alert("ERROR: EMPTY TWEET")
+      document.querySelector('.error').innerHTML = 'Error, Empty tweet.'
+      $('.error').show()
+      console.log("empty tweet")
       return false
     }
-  else { 
+    else {
 
-    $.post('/tweets', serializedData, (response) => {
-      $("#form-new-tweet").trigger("reset")
-      fetchTweets();
+      $.post('/tweets', serializedData, (response) => {
+        console.log(response)
+        $("#tweet-text").val('');
+        $(".counter").val('140');
+        fetchTweets();
+      });
 
-     });
-
-  }
+    }
 
   });
 
-
-  const renderTweets = function(tweets) {
-    $("#tweets").empty();
-    for (const tweet of tweets) {
-      const $newTweet = createTweetElement(tweet)
-      $("#tweets").prepend($newTweet)
-    }
-    
-    }
-    
-    const createTweetElement = function(tweetData) {
-    let $tweet =`
-    <article class="tweet">
-        <header class="tweet">
-          <div class="tweet">
-            <img height="50px" width="50px" class="avatars" src="${tweetData.user.avatars}">
-            <p>${tweetData.user.name}</p>
-          </div>
-          <p class="handle">${tweetData.user.handle}</p>
-        </header>
-    
-        <main class="content">
-          ${tweetData.content.text}
-        </main>
-    
-        <footer class="tweet">
-          <p class="created_at">${timeago.format(tweetData.created_at)}</p>
-          <div class="icons">
-            <div class="flag"><i class="fas fa-flag"></i></div>
-            <div class="retweet"><i class="fas fa-retweet"></i></div>
-            <div class="like"><i class="fas fa-heart"></i></div>  
-          </div>
-        </footer>
-    
-        </article>
-    `
-    
-    // ...
-    return $tweet;
-    }
-    
-    renderTweets(data);
-
-
-
-
-
 });
-
-
-
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,26 +1,21 @@
 $(document).ready(function() {
     //Count characters of tweet
+    
     $('#tweet-text').on('input', function() {
      let maxChar = 140;
      console.log($(this).val())
      const inputData = $(this).val().length;
-    
      const remainingChar = maxChar - inputData;
      const $counter = $('#form-new-tweet').children('.submit').children('.counter');
-     //const $counter = $(this).parent("#form-new-tweet")
-
     $counter.text(remainingChar); 
+
     // style remaining characters
     if(remainingChar < 0) {
       $counter.css('color', 'red');
     } else {
       $counter.css('color', 'black');
     }
-    
-      
   })
   
   });
-  
-  
   console.log('CONNECTED!!')
